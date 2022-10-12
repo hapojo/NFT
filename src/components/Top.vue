6e94df23973f5ace2969ac03328012f9f29d6450
Happiness ojo <48913853+hapojo@users.noreply.github.com>
2022-10-12 11:03:42 +0100

    top bar

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,12 +1,41 @@
 <template>
-    <div>
-        <input type="text" name="search" placeholder="Search Item">
+    <div class="top-content">
+        <div class="search">
+            <input type="text" name="search" placeholder="Search Items">
+        </div>
+
+        <div class="button b2" id="button-10">
+            <input type="checkbox" class="checkbox">
+            <div class="knobs">
+                <span>Users</span>
+            </div>
+            <div class="layer"></div>
+        </div>
+
+        <div>
+            <div class="notification">
+                <span><img src="../assets/image/notification.svg" alt=""></span>
+            </div>
+
+            <div class="profile">
+                <span><img src="../assets/image/profile.svg" alt=""></span>
+            </div>
+        </div>
+
     </div>
+
+
 </template>
 
 
 <style>
-input {
+.top-content {
+    display: flex;
+    justify-content: space-between;
+    padding-right: 40px;
+}
+
+.search input {
     width: 350px;
     height: 40px;
     color: #FFFFFF !important;
@@ -20,7 +49,132 @@ input {
     padding: 12px 20px 12px 40px;
 }
 
-input:hover {
-    border: #595D7C;
+::placeholder {
+    color: #FFFFFF;
+}
+
+
+.knobs,
+.layer {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+
+.button {
+    position: relative;
+    width: 200px;
+    height: 39px;
+    overflow: hidden;
+    margin-left: 300px;
+}
+
+.button.r,
+.button.r .layer {
+    border-radius: 100px;
+}
+
+.button.b2 {
+    border-radius: 100px;
+}
+
+.checkbox {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    opacity: 0;
+    cursor: pointer;
+    z-index: 3;
+}
+
+.knobs {
+    z-index: 2;
+}
+
+.layer {
+    width: 100%;
+    background-color: #C4C4C4;
+    transition: 0.3s ease all;
+    z-index: 1;
+}
+
+/* Button 10 */
+#button-10 .knobs:before,
+#button-10 .knobs:after,
+#button-10 .knobs span {
+    position: absolute;
+    top: 4px;
+    width: 100px;
+    height: 30px;
+    font-size: 10px;
+    font-weight: bold;
+    text-align: center;
+    line-height: 1;
+    padding: 9px 4px;
+    border-radius: 100px;
+    transition: 0.3s ease all;
+}
+
+#button-10 .knobs:before {
+    content: '';
+    left: 4px;
+    background-color: #22295A;
+}
+
+#button-10 .knobs:after {
+    content: 'Customers';
+    right: 4px;
+    color: #FFFFFF;
+}
+
+#button-10 .knobs span {
+    display: inline-block;
+    left: 4px;
+    color: #FFFFFF;
+    z-index: 1;
+}
+
+#button-10 .checkbox:checked+.knobs span {
+    color: #FFFFFF;
+}
+
+#button-10 .checkbox:checked+.knobs:before {
+    left: 95px;
+    background-color: #22295A;
+}
+
+#button-10 .checkbox:checked+.knobs:after {
+    color: #FFFFFF;
+}
+
+#button-10 .checkbox:checked~.layer {
+    background-color: #C4C4C4;
+}
+
+.notification {
+    background-color: #777B95;
+    color: white;
+    padding: 7px 10px;
+    margin-right: 10px;
+    position: relative;
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    border-radius: 100px;
+}
+
+.notification:hover {
+    background: #777B95;
+}
+
+.profile {
+    padding: 5px 5px;
+    position: relative;
+    display: inline-block;
+    border-radius: 100px;
 }
 </style>
